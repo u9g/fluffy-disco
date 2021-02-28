1a803aae7f91780319fcecf2fb9b3b84745d5cd8
U9G <winworkswow@gmail.com>
2021-02-27 22:04:55 -0500

    made tags be grabbed from json file

--- index.js
+++ index.js
@@ -1,66 +1,52 @@
 require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const tags = require('./tags.json')
-const fs = require('fs').promises
+const fetch = require('node-fetch')
+let tags = null
 
-client.on('ready', () => {
+client.on('ready', async () => {
+  await reloadTags()
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', async (msg) => {
-  if (msg.content.startsWith('?tag') || msg.content.startsWith('?tags')) {
+  if (msg.content.startsWith('?tags')) {
     const [, ...args] = msg.content.split(' ')
     switch (args[0]) {
-      case 'make':
+      case 'reload':
         if (!hasPermission(msg)) {
-          msg.channel.send("You don't have permission to modify the tags database. (You need `tags` role)")
-          break
-        } else if (tags[args[1]]) {
-          msg.channel.send(`Tag already exists, use \`?tag delete ${args[1]}\` first to reuse this tag name.`)
-          break
-        } else if (args[1] === 'tag' || args[1] === 'tags') {
-          msg.channel.send('Invalid tag, try again with a different tag name.')
-          break
-        } else if (!args[2]) {
-          msg.channel.send(`A tag needs a message, rerun the command with a message: \`?tag make ${args[1]} [message here]\``)
+          msg.channel.send("You don't have permission to reload the tags database. (You need `tags` role)")
           break
         }
-        await makeTag(args, msg)
-        msg.channel.send('Tag made successfully!')
-        break
-      case 'delete':
-      case 'del':
-        if (!hasPermission(msg)) {
-          msg.channel.send("You don't have permission to modify the tags database. (You need `tags` role)")
-          break
+        try {
+          await reloadTags()
+          msg.channel.send('Tag reload successful.')
+        } catch (err) {
+          msg.channel.send('Tag reload failed.')
         }
-        await delTag(args)
-        msg.channel.send('Tag deleted successfully')
         break
       case 'list':
         getTags().then(res => msg.channel.send(res))
         break
-      default: // ?tag help
+      default:
         msg.channel.send(
-          '* `?tag make [name] [msg]` to make a tag' +
-        '\n* `?tag del[ete] [name]` to delete a tag' +
-         '\n* `?tag list` to list all tags' +
+          '* `?tags reload` to reload the tags' +
+         '\n* `?tags list` to list all tags' +
           '\n* `?[tag]` to use a tag')
         break
     }
   } else {
     if (!msg.content.startsWith('?')) return
-    const tagMessage = getTag(msg.content.substring(1))
-    msg.channel.send(tagMessage)
+    if (tags[msg.content.substring(1)]) {
+      msg.channel.send(tags[msg.content.substring(1)].message)
+    }
   }
 })
 
 async function getTags () {
   const text = Object.entries(tags)
   for (let i = 0; i < text.length; i++) {
-    const user = await client.users.fetch(text[i][1].createdBy)
-    text[i] = `${i + 1}. \`?${text[i][0]}\` by ${user.username}`
+    text[i] = `${i + 1}. \`?${text[i][0]}\``
   }
   return text.join('\n')
 }
@@ -69,29 +55,9 @@ function hasPermission (msg) {
   return msg.member.roles.cache.some(role => role.name === 'tags')
 }
 
-function getTag (name) {
-  return tags[name].message
-}
-
-async function makeTag (args, msg) {
-  let [, tagName, ...tagMessage] = args
-  tagMessage = Discord.escapeMarkdown(tagMessage.join(' '))
-  tags[tagName] = {
-    message: tagMessage,
-    createdBy: msg.author.id,
-    createdDate: Date.now()
-  }
-  await save()
-}
-
-async function delTag (args) {
-  const [, tagName] = args
-  delete tags[tagName]
-  await save()
-}
-
-async function save () {
-  await fs.writeFile('./tags.json', JSON.stringify(tags, null, 2))
+async function reloadTags () {
+  const fetched = await fetch(process.env.JSONURL)
+  tags = await fetched.json()
 }
 
 client.login(process.env.TOKEN)
