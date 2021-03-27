8b2f3777d54b85c6392ee1c95f33d65f69a3a4af
u9g <43508353+u9g@users.noreply.github.com>
2021-03-27 01:32:21 -0400

    adsad

--- index.js
+++ index.js
@@ -12,45 +12,34 @@ client.on('ready', async () => {
 client.on('message', async (msg) => {
   if (msg.content.startsWith('?tags')) {
     const [, ...args] = msg.content.split(' ')
-    switch (args[0]) {
-      case 'reload':
-        if (!hasPermission(msg)) {
-          msg.channel.send("You don't have permission to reload the tags database. (You need `tags` role)")
-          break
-        }
+    const [subCommand] = args
+
+    if (subCommand === 'reload') {
+      if (hasPermission(msg)) {
         try {
           await reloadTags()
+          setTimeout(reloadTags, 15 * 60 * 1000)
+
           msg.channel.send('Tag reload successful.')
         } catch (err) {
           msg.channel.send('Tag reload failed.')
         }
-        break
-      case 'list':
-        getTags().then(res => msg.channel.send(res))
-        break
-      default:
-        msg.channel.send(tags.help)
-        break
-    }
+      } else msg.channel.send("You don't have permission to reload the tags database. (You need `tags` role)")
+    } else if (subCommand === 'list') msg.channel.send(await getTags())
+    else msg.channel.send(tags.help)
   } else {
     if (!msg.content.startsWith('?')) return
     if (tags[msg.content.substring(1)]) {
       const tag = tags[msg.content.substring(1)]
-      if (tag.type === 'message') {
-        msg.channel.send(tag.message)
-      } else if (tag.type === 'image') {
-        msg.channel.send({ files: [tag.src] })
-      }
+      msg.channel.send(tag)
     }
   }
 })
 
 async function getTags () {
-  const text = Object.entries(tags)
-  for (let i = 0; i < text.length; i++) {
-    text[i] = `${i + 1}. \`?${text[i][0]}\``
-  }
-  return text.join('\n')
+  return Object.keys(tags)
+    .map((cmdName, i) => `${i + 1}. \`?${cmdName}\``)
+    .join('\n')
 }
 
 function hasPermission (msg) {
@@ -58,8 +47,7 @@ function hasPermission (msg) {
 }
 
 async function reloadTags () {
-  const fetched = await fetch(process.env.JSONURL)
-  tags = await fetched.json()
+  tags = await (await fetch(process.env.JSONURL)).json()
 }
 
 client.login(process.env.TOKEN)
